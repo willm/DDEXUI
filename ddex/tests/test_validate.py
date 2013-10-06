32c0a89699907da67738d3e4cfa6e14545756998
Will Munn <willmunn@hotmail.com>
2013-10-06 23:18:27 +0100

    validated year

--- ddex/tests/test_validate.py
+++ ddex/tests/test_validate.py
@@ -8,12 +8,20 @@ class ValidateTests(unittest.TestCase):
 
 	def test_upcs_cannot_contain_less_than_12_digits(self):
 		error = Validate().upc("12345")
-		self.assertEqual(error, "upc must be 12 - 13 digit long")
+		self.assertEqual(error, "upc must be 12 - 13 digits long")
 
 	def test_upcs_cannot_contain_more_than_13_digits(self):
 		error = Validate().upc("12345678910235")
-		self.assertEqual(error, "upc must be 12 - 13 digit long")
+		self.assertEqual(error, "upc must be 12 - 13 digits long")
 
 	def test_valid_upcs_do_not_return_errors(self):
 		error = Validate().upc("123456789012")
+		self.assertEqual(error, None)
+
+	def test_year_must_be_a_number(self):
+		error = Validate().year("a")
+		self.assertEqual(error, "year must be a number")
+
+	def test_a_valid_year_doesn_not_return_any_errors(self):
+		error = Validate().year("2012")
 		self.assertEqual(error, None)
--- ddex/validate.py
+++ ddex/validate.py
@@ -3,10 +3,19 @@ class Validate:
 	
 	def upc(self, text):
 		if(len(text) < 12 or len(text) > 13):
-			return "upc must be 12 - 13 digit long"
+			return "upc must be 12 - 13 digits long"
 		for char in text:
-			try:
-				int(char)
-				break
-			except ValueError:
+			if(not self.__number(char)):
 				return "upc must only contain numbers"
+	
+	def year(self, text):
+		if(not self.__number(text)):
+			return "year must be a number"
+
+	def __number(self, text):
+		try:
+			int(text)
+			return True
+		except ValueError:
+			return False
+	
