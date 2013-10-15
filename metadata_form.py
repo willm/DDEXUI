83c6e3950efa11d0abdd3e8bce4eb006a564c645
Will Munn <willmunn@hotmail.com>
2013-10-15 07:47:53 +0100

    experimenting with validation callbacks in UI

--- metadata_form.py
+++ metadata_form.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from DDEXUI.ddex.productRelease import ProductRelease
+from DDEXUI.ddex.ddex import DDEX
 
 class Program:
 
@@ -6,22 +8,46 @@ class Program:
 		self.values = {}
 		self.top = tk.Tk()
 		self.top.title("Metadata Editor")
-		self.fields = ["Title", "Artist", "UPC", "Year", "C Line", "P Line"]
+		self.fields = ["Title", "UPC", "Year", "C Line", "P Line"]
 
-	def addEntry(self, row, title):
+	def on_invalidate(self, row):
+		tk.Label(self.top, text="wrong").grid(row=row, column=2)
+
+	def on_validate(self, S):
+		print("validating"+S)
+		return S == "abc"
+
+	def add_entry(self, row, title):
 		text = tk.StringVar()
 		self.values[title] = text
-		entry = tk.Entry(self.top,width=10, textvariable=text).grid(row=row, column=0)
+		vcmd = (self.top.register(self.on_validate), '%S')
+		entry = tk.Entry(self.top,width=10, textvariable=text, validate="focusout", validatecommand=vcmd, invalidcommand=lambda: self.on_invalidate(row)).grid(row=row, column=0)
 		label = tk.Label(self.top,text=title).grid(row=row, column=1)
 
-	def doit(self):
-		for key, value in self.values.items():
-			print(key, value.get())
+	def create_ddex(self):
+		if(self.valid()):
+			product_release = build_product_release()
+			DDEX(product_release).write()
+
+	def build_product_release(self):
+		product_release = ProductRelease()
+		product_release.product_name = self.value_of("Title")
+		product_release.upc = self.value_of("UPC")
+		product_release.cline = self.value_of("C Line")
+		product_release.pline = self.value_of("P Line")
+		product_release.year = self.value_of("Year")
+		return product_release
+
+	def valid(self):
+		return True
+
+	def value_of(self, key):
+		return self.values[key].get()
 
 	def main(self):
 		for i in range(0,len(self.fields)):
-			self.addEntry(i, self.fields[i])
-		tk.Button(self.top, text="OK", command=self.doit).grid(row=len(self.fields), column=0)
+			self.add_entry(i, self.fields[i])
+		tk.Button(self.top, text="OK", command=self.create_ddex).grid(row=len(self.fields), column=0)
 		self.top.mainloop()
 
 Program().main()
