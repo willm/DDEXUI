82667cb2f9671634918206ea42a64958ddb3a341
Will Munn <willmunn@hotmail.com>
2013-10-23 00:12:46 +0100

    added checkox input for explicit

--- metadata_form.py
+++ metadata_form.py
@@ -20,7 +20,8 @@ class Program:
 			EntryInput(self.top, "P Line", Validate().not_empty),
 			EntryInput(self.top, "Artist", Validate().not_empty),
 			EntryInput(self.top, "Label", Validate().not_empty),
-			OptionInput(self.top, "Type", Validate().not_empty, 'Single', 'Album')
+			OptionInput(self.top, "Type", 'Single', 'Album'),
+			CheckboxInput(self.top, "Explicit")
 		])
 		self.button = tk.Button(self.top, text="OK", command=self.create_ddex)
 
@@ -44,7 +45,7 @@ class Program:
 			self.value_of("Type"),
 			self.value_of("Artist"),
 			self.value_of("Label"),
-			"False"))
+			self.value_of("Explicit")))
 
 	def value_of(self, title):
 		row = next(filter(lambda x: x.title == title,self.fields))
@@ -59,52 +60,69 @@ class Program:
 		self.top.mainloop()
 
 class InputRow:
-	def __init__(self, frame, title, validation_function):
+	def __init__(self, frame, title):
 		self.frame = frame
-		self.validation_function = validation_function
 		self.error_label = tk.tkinter.Label(self.frame, fg="red", width=50)
-		self.text = tk.tkinter.StringVar()
-		self.label = tk.Label(self.frame,text=title)
+		self.v = tk.tkinter.StringVar()
 		self.title = title
 		self.input = None
 
+	def value(self):
+		return self.v.get()
+
+	def on_validate(self):
+		return True
+
 	def draw(self, row):
-		self.label.grid(row=row, column=0)
 		self.input.grid(row=row, column=1)
 		self.error_label.grid(row=row, column=2)
+
+class OptionInput(InputRow):
+	def __init__(self, frame, title, *args):
+		InputRow.__init__(self, frame, title)
+		self.v.set(args[0])
+		self.input = tk.OptionMenu(self.frame, self.v, args[0], *args)
+		self.label = tk.Label(self.frame,text=title)
+
+	def draw(self,row):
+		InputRow.draw(self, row)
+		self.label.grid(row=row, column=0)
+	
+class CheckboxInput(InputRow):
+	def __init__(self, frame, title):
+		InputRow.__init__(self, frame, title)
+		self.v = tk.tkinter.BooleanVar()
+		self.v.set(False)
+		self.input = tk.Checkbutton(self.frame, variable=self.v, text=title)
+
+class EntryInput(InputRow):
+	def __init__(self, frame, title, validation_function):
+		InputRow.__init__(self, frame, title)
+		self.validation_function = validation_function
+		self.label = tk.Label(self.frame,text=title)
+		self.input = (tk.Entry(
+			self.frame,
+			width=20, 
+			textvariable=self.v, 
+			validate="focusout", 
+			validatecommand=self.on_validate,
+			invalidcommand=lambda: self.on_invalidate(self.validation_function(self.v.get())["error"])))
+		self.v.set("2121211111141")
+
+	def is_valid(self):
+		return self.validation_function(self.v.get())["success"]	== True
+
+	def draw(self,row):
+		InputRow.draw(self, row)
+		self.label.grid(row=row, column=0)
+
+	def on_invalidate(self, message):
+		self.error_label["text"] = message
 
 	def on_validate(self):
 		valid = self.is_valid()
 		if(valid):
 			self.error_label["text"] = ""
 		return valid
-
-	def is_valid(self):
-		return self.validation_function(self.text.get())["success"]	== True
-
-	def value(self):
-		return self.validation_function(self.text.get())["value"]
-
-	def on_invalidate(self, message):
-		self.error_label["text"] = message
-
-class OptionInput(InputRow):
-	def __init__(self, frame, title, validation_function, *args):
-		InputRow.__init__(self, frame, title, validation_function)
-		self.text.set(args[0])
-		self.input = tk.OptionMenu(self.frame, self.text, args[0], *args)
-		
-
-class EntryInput(InputRow):
-	def __init__(self, frame, title, validation_function):
-		InputRow.__init__(self, frame, title, validation_function)
-		self.input = (tk.Entry(
-			self.frame,
-			width=20, 
-			textvariable=self.text, 
-			validate="focusout", 
-			validatecommand=self.on_validate,
-			invalidcommand=lambda: self.on_invalidate(self.validation_function(self.text.get())["error"])))
-		self.text.set("2121211111141")
 
 Program().main()
