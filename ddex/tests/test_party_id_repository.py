a8ae3317e2045f83aaf3274337059197f8792a7a
will m <william.munn@7digital.com>
2013-10-23 18:01:28 +0100

    few more tests around party id repo

--- ddex/tests/test_party_id_repository.py
+++ ddex/tests/test_party_id_repository.py
@@ -5,25 +5,49 @@ class PartyIdRepositoryTests(unittest.TestCase):
 	def setUp(self):
 		self.config = configparser.RawConfigParser()
 		self.config.add_section('Sender')
-
-	def test_it_should_notify_if_a_party_id_exists(self):
-		self.config.set('Sender', 'party_id', 'LETSHAVEAPARTY')
-		with open('ddexui.cfg', 'w') as configfile:
-			self.config.write(configfile)
+		self.party_id = 'LETSHAVEAPARTY'
+		self.config.set('Sender', 'party_id', self.party_id)
 		
-		self.assertTrue(PartyIdRepository().party_id_exists())
+	def test_it_should_return_the_party_id(self):
+		self.__write_config()
 		
-	def test_it_should_notify_if_no_party_id_exists(self):
+		self.assertEqual(PartyIdRepository().get_party_id(), self.party_id)
+		
+	def test_it_should_return_none_if_there_is_no_party_id(self):
 		self.config.remove_option('Sender', 'party_id')
+		self.__write_config()
+		
+		self.assertEqual(PartyIdRepository().get_party_id(), None)
+	
+	@unittest.skip("work in progress")
+	def test_it_should_write_the_party_id(self):
+		self.config.remove_option('Sender', 'party_id')
+		repo = PartyIdRepository()
+		repo.write_party_id(self.party_id)
+		
+		self.assertEqual(repo.get_party_id(), self.party_id)
+		
+
+	def __write_config(self):
 		with open('ddexui.cfg', 'w') as configfile:
 			self.config.write(configfile)
-		
-		self.assertFalse(PartyIdRepository().party_id_exists())
-
 		
 		
 class PartyIdRepository:
-	def party_id_exists(self):
-		config = configparser.RawConfigParser()
-		config.read('ddexui.cfg')
-		return config.has_option('Sender', 'party_id')
+	def __init__(self):
+		self.config = configparser.RawConfigParser()
+
+	def __read_config_file(self):
+		self.config.read('ddexui.cfg')
+		
+	def __party_id_exists(self):
+		self.__read_config_file();
+		return self.config.has_option('Sender', 'party_id')
+		
+	def get_party_id(self):
+		if(self.__party_id_exists()):
+			return self.config.get('Sender', 'party_id')
+		return None
+		
+	def write_party_id(self):
+		pass
