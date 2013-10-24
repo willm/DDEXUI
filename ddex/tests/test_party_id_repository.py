f66121404dc6d977b886d4f68a3ba484ce3c9c54
Will Munn <willmunn@hotmail.com>
2013-10-24 01:03:52 +0100

    expanded party object

--- ddex/tests/test_party_id_repository.py
+++ ddex/tests/test_party_id_repository.py
@@ -5,34 +5,51 @@ class PartyRepositoryTests(unittest.TestCase):
 	def setUp(self):
 		self.config = configparser.RawConfigParser()
 		self.config.add_section('Sender')
-		self.party_id = 'LETSHAVEAPARTY'
-		self.config.set('Sender', 'party_id', self.party_id)
+		self.party = Party('LETSHAVEAPARTY', 'Some Label Name')
+		self.config.set('Sender', 'party_id', self.party.party_id)
+		self.config.set('Sender', 'name', self.party.name)
 		
-	def test_it_should_return_the_party_id(self):
+	def test_it_should_return_the_party(self):
 		self.__write_config()
 		
-		self.assertEqual(PartyRepository().get_party_id(), self.party_id)
+		self.assertEqual(PartyRepository().get_party(), self.party)
 		
-	def test_it_should_return_none_if_there_is_no_party_id(self):
+	def test_it_should_return_none_if_there_is_no_party(self):
 		self.config.remove_option('Sender', 'party_id')
 		self.__write_config()
 		
-		self.assertEqual(PartyRepository().get_party_id(), None)
+		self.assertEqual(PartyRepository().get_party(), None)
 	
-	def test_it_should_write_the_party_id(self):
+	def test_it_should_write_the_party(self):
 		self.config.remove_option('Sender', 'party_id')
+		self.config.remove_option('Sender', 'name')
 		self.__write_config()
 		repo = PartyRepository()
-		repo.write_party_id(self.party_id)
+		repo.write_party(self.party)
 		
-		self.assertEqual(repo.get_party_id(), self.party_id)
+		self.assertEqual(repo.get_party(), self.party)
 		
 
 	def __write_config(self):
 		with open('ddexui.cfg', 'w') as configfile:
 			self.config.write(configfile)
 
-		
+class Party:
+	def __init__(self, party_id, name):
+		self.party_id = party_id
+		self.name = name
+
+	def __eq__(self, other):
+		if(isinstance(other, Party)):
+			return self.name == other.name and self.party_id == other.party_id
+		return NotImplemented
+
+	def __ne__(self, other):
+		result = self.__eq__(other)
+		if(result is NotImplemented):
+			return result
+		return not result
+
 class PartyRepository:
 	def __init__(self):
 		self.config = configparser.RawConfigParser()
@@ -40,17 +57,18 @@ class PartyRepository:
 	def __read_config_file(self):
 		self.config.read('ddexui.cfg')
 		
-	def __party_id_exists(self):
+	def __party_exists(self):
 		self.__read_config_file();
 		return self.config.has_option('Sender', 'party_id')
 		
-	def get_party_id(self):
-		if(self.__party_id_exists()):
-			return self.config.get('Sender', 'party_id')
+	def get_party(self):
+		if(self.__party_exists()):
+			return Party(self.config.get('Sender', 'party_id'), self.config.get('Sender', 'name'))
 		return None
 		
-	def write_party_id(self, party_id):
+	def write_party(self, party):
 		self.config.add_section('Sender')
-		self.config.set('Sender', 'party_id', party_id)
+		self.config.set('Sender', 'party_id', party.party_id)
+		self.config.set('Sender', 'name', party.name)
 		with open('ddexui.cfg', 'w') as configfile:
 			self.config.write(configfile)
