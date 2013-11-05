bfb6ef80c4f0e8cb8dc3a4e87b0bfec7d463eb2b
Will Munn <willmunn@hotmail.com>
2013-11-05 22:38:10 +0000

    parsed dates and territory code

--- ddex/deal.py
+++ ddex/deal.py
@@ -1,8 +1,13 @@
 import xml.etree.cElementTree as ET
+import datetime as date
 
 class Deal:
 
-	def __init__(self, commercial_model, use_type):
+	def __init__(self, commercial_model, use_type, territory, start_date, preorder_date=None, preorder_preview_date=None):
+		self.start_date = start_date
+		self.preorder_date = preorder_date
+		self.preorder_preview_date = preorder_preview_date
+		self.territory = territory
 		self.commercial_model = commercial_model
 		self.use_type = use_type
 
@@ -12,6 +17,11 @@ class Deal:
 		self.__append_element_with_text(terms, "CommercialModelType", self.commercial_model)
 		usage = self.__append_element_with_text(terms, "Usage")
 		self.__append_element_with_text(usage, "UseType", self.use_type)
+		self.__append_element_with_text(terms, "TerritoryCode", self.territory)
+		self.__append_element_with_text(terms, "PreorderReleaseDate", self.preorder_date.isoformat())
+		self.__append_element_with_text(terms, "PreorderPreviewDate", self.preorder_preview_date.isoformat())
+		validity_period = self.__append_element_with_text(terms, "ValidityPeriod")
+		self.__append_element_with_text(validity_period, "StartDate", self.start_date.isoformat())
 		return deal
 		
 	def __append_element_with_text(self, parent, name, text=""):
--- ddex/tests/test_deal.py
+++ ddex/tests/test_deal.py
@@ -1,12 +1,17 @@
 import unittest
 import xml.etree.cElementTree as ET
 from DDEXUI.ddex.deal import Deal
+from datetime import date
 
 class DealTests(unittest.TestCase):
 
 	def setUp(self):
 		self.use_type = "PermanentDownload"
-		deal = Deal("PayAsYouGoModel", self.use_type)
+		self.territory = "BE"
+		self.start_date = date(1987,2,20)
+		self.preorder_date = date(1987,2,19)
+		self.preorder_preview_date = date(1987,2,18)
+		deal = Deal("PayAsYouGoModel", self.use_type, self.territory, self.start_date, self.preorder_date, self.preorder_preview_date)
 		self.element = deal.write()
 
 	def test_should_have_commercial_model_type(self):
@@ -14,3 +19,15 @@ class DealTests(unittest.TestCase):
 
 	def test_should_have_use_type(self):
 		self.assertEqual(self.element.find("./DealTerms/Usage/UseType").text, self.use_type)
+
+	def test_should_have_territory_code(self):
+		self.assertEqual(self.element.find("./DealTerms/TerritoryCode").text, self.territory)
+
+	def test_should_have_start_date(self):
+		self.assertEqual(self.element.find("./DealTerms/ValidityPeriod/StartDate").text, "1987-02-20") 
+
+	def test_should_have_preorder_date(self):
+		self.assertEqual(self.element.find("./DealTerms/PreorderReleaseDate").text, "1987-02-19") 
+
+	def test_should_have_preorder_preview_date(self):
+		self.assertEqual(self.element.find("./DealTerms/PreorderPreviewDate").text, "1987-02-18") 
