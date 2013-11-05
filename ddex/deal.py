760aac3433723a454dd67bba8f488ec7d08954b7
Will Munn <willmunn@hotmail.com>
2013-11-05 21:01:57 +0000

    parsed use type

--- ddex/deal.py
+++ ddex/deal.py
@@ -2,13 +2,16 @@ import xml.etree.cElementTree as ET
 
 class Deal:
 
-	def __init__(self, commercial_model):
+	def __init__(self, commercial_model, use_type):
 		self.commercial_model = commercial_model
+		self.use_type = use_type
 
 	def write(self):
 		deal = ET.Element("Deal")
 		terms = self.__append_element_with_text(deal, "DealTerms")
 		self.__append_element_with_text(terms, "CommercialModelType", self.commercial_model)
+		usage = self.__append_element_with_text(terms, "Usage")
+		self.__append_element_with_text(usage, "UseType", self.use_type)
 		return deal
 		
 	def __append_element_with_text(self, parent, name, text=""):
--- ddex/tests/test_deal.py
+++ ddex/tests/test_deal.py
@@ -5,9 +5,12 @@ from DDEXUI.ddex.deal import Deal
 class DealTests(unittest.TestCase):
 
 	def setUp(self):
-		deal = Deal("PayAsYouGoModel")
+		self.use_type = "PermanentDownload"
+		deal = Deal("PayAsYouGoModel", self.use_type)
 		self.element = deal.write()
 
 	def test_should_have_commercial_model_type(self):
 		self.assertEqual(self.element.find("./DealTerms/CommercialModelType").text, "PayAsYouGoModel")
 
+	def test_should_have_use_type(self):
+		self.assertEqual(self.element.find("./DealTerms/Usage/UseType").text, self.use_type)
