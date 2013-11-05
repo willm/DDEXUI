668b7fb14948235a9d7db6bd137d9c80ac84bfae
Will Munn <willmunn@hotmail.com>
2013-11-05 23:42:21 +0000

    fixed casing inconsistency

--- ddex/ddex.py
+++ ddex/ddex.py
@@ -3,7 +3,7 @@ from DDEXUI.ddex.message_header import MessageHeader
 
 class DDEX:
 
-	def __init__(self, sender, product_release, resources):
+	def __init__(self, sender, product_release, resources=[]):
 		self.product_release = product_release
 		self.resources = resources
 		self.sender = sender
@@ -11,15 +11,15 @@ class DDEX:
 	def write(self):
 		root =  ET.Element("ernm:NewReleaseMessage", {'MessageSchemaVersionId': 'ern/341', 'LanguageAndScriptCode': 'en', 'xs:schemaLocation': 'http://ddex.net/xml/ern/341 http://ddex.net/xml/ern/341/release-notification.xsd', 'xmlns:ernm': 'http://ddex.net/xml/ern/341', 'xmlns:xs':'http://www.w3.org/2001/XMLSchema-instance'})
 		header = self.__write_message_header(root)
-		releaseList = ET.SubElement(root,"ReleaseList")
-		releaseList.append(self.__write_product_release())
+		release_list = ET.SubElement(root,"ReleaseList")
+		release_list.append(self.__write_product_release())
 
 		resource_list = ET.SubElement(root,"ResourceList")
 		for resource in self.resources:
 			resource_list.append(resource.write())
 
-		dealList = ET.SubElement(root,"DealList")
-		dealList.append(self.product_release.write_deals())
+		deal_list = ET.SubElement(root,"DealList")
+		deal_list.append(self.product_release.write_deals())
 		
 		tree = ET.ElementTree(root)
 		tree.write("/tmp/file.xml")
