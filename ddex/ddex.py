c19ad7d392a86eb974418f33ed63fe5e2fc019b3
Will Munn <willmunn@hotmail.com>
2013-11-05 23:39:21 +0000

    added resource list

--- ddex/ddex.py
+++ ddex/ddex.py
@@ -1,11 +1,11 @@
 import xml.etree.cElementTree as ET
 from DDEXUI.ddex.message_header import MessageHeader
-import datetime as d
 
 class DDEX:
 
-	def __init__(self, sender, product_release):
+	def __init__(self, sender, product_release, resources):
 		self.product_release = product_release
+		self.resources = resources
 		self.sender = sender
 
 	def write(self):
@@ -14,11 +14,15 @@ class DDEX:
 		releaseList = ET.SubElement(root,"ReleaseList")
 		releaseList.append(self.__write_product_release())
 
+		resource_list = ET.SubElement(root,"ResourceList")
+		for resource in self.resources:
+			resource_list.append(resource.write())
+
 		dealList = ET.SubElement(root,"DealList")
 		dealList.append(self.product_release.write_deals())
 		
 		tree = ET.ElementTree(root)
-		tree.write("file.xml")
+		tree.write("/tmp/file.xml")
 	
 	def __write_product_release(self):
 		return self.product_release.write()
--- ddex/tests/test_validates_against_ddex_schema.py
+++ ddex/tests/test_validates_against_ddex_schema.py
@@ -3,6 +3,7 @@ from DDEXUI.ddex.ddex import DDEX
 from DDEXUI.ddex.release import Release, ReleaseId
 from DDEXUI.ddex.party import Party
 from DDEXUI.ddex.deal import Deal
+from DDEXUI.ddex.resource import SoundRecording
 from datetime import date
 import unittest
 
@@ -25,10 +26,12 @@ class DDEXSchemaValidation(unittest.TestCase):
 		deal = Deal("PayAsYouGoModel", "PermenantDownload", "FR", date(2012,1,3))
 
 		release.add_deal(deal)
-
-		DDEX(Party('derwwfefw', 'Sony'),release).write()
 		
-		tree = ET.parse('file.xml')
+		resources = [SoundRecording("abc","ddex/tests/resources/test.mp3")]
+
+		DDEX(Party('derwwfefw', 'Sony'),release, resources).write()
+		
+		tree = ET.parse('/tmp/file.xml')
 #		tree = ET.parse('/home/will/Documents/python/DDEXUI/ddex/tests/resources/ddex-sample.xml')
 		schema = ET.XMLSchema(file="http://ddex.net/xml/ern/341/release-notification.xsd")
 		schema.assertValid(tree)
