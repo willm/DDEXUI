31ae44995286c0454f8dc0012159cd06839e5be1
Will Munn <willmunn@hotmail.com>
2013-11-28 19:50:16 +0000

    deal window now creates a deal. needs refactoring

--- metadata_form.py
+++ metadata_form.py
@@ -7,19 +7,48 @@ from DDEXUI.ddex.ddex import DDEX
 from DDEXUI.ddex.party import *
 from DDEXUI.ddex.validate import Validate
 from DDEXUI.party_repository import PartyRepository
+from  datetime import datetime as datetime
 import DDEXUI.ddex.deal as deal
 
 class DealWindow(tk.tkinter.Toplevel):
 	def __init__(self, frame):
 		tk.tkinter.Toplevel.__init__(self, frame)
+		self.title("Deal Editor")
 		self.focus_set()
 		self.fields = ([OptionInput(self, "Commercial Model", *deal.CommercialModals),
 			OptionInput(self, "Use Type", *deal.UseTypes),
 			OptionInput(self, "Territory", *deal.Territories),
-			EntryInput(self, "Start Date", Validate().not_empty)])
-		for i in range(len(self.fields) - 1):
+			EntryInput(self, "Start Date", Validate().not_empty),
+			EntryInput(self, "Pre Order Date", Validate().not_empty),
+			EntryInput(self, "Pre Order Preview Date", Validate().not_empty)])
+		for i in range(len(self.fields)):
 			self.fields[i].draw(i)
-		tk.Button(self, text="OK", command=self.destroy).grid(row=len(self.fields), column=0)
+		tk.Button(self, text="OK", command=self.__destroy_if_valid).grid(row=len(self.fields)+1, column=0)
+
+	def __destroy_if_valid(self):
+		if(self.all_release_fields_valid()):
+			self.destroy()	
+
+	def create_deal(self):
+		#massive hack sort out this ugly date parsing
+		date_format = "%Y-%m-%d"
+		return (deal.Deal(self.value_of("Commercial Model"),
+			self.value_of("Use Type"),
+			self.value_of("Territory"),
+			datetime.strptime(self.value_of("Start Date"), date_format),
+			datetime.strptime(self.value_of("Pre Order Date"), date_format),
+			datetime.strptime(self.value_of("Pre Order Preview Date"), date_format)))
+
+#todo: remove duplication of these 2 methods
+	def value_of(self, title):
+		row = next(filter(lambda x: x.title == title,self.fields))
+		return row.value()
+
+	def all_release_fields_valid(self):
+		all_valid = True
+		for row in self.fields:
+			all_valid = all_valid and row.on_validate()
+		return all_valid
 
 class PartyWindow(tk.tkinter.Toplevel):
 	def __init__(self, frame, party_type):
@@ -67,8 +96,14 @@ class Program:
 			OptionInput(self.top, "Type", 'Single', 'Album'),
 			CheckboxInput(self.top, "Explicit")
 		])
-		self.add_deal_button = tk.Button(self.top, text="Add deal", command=lambda:DealWindow(self.top))
+		self.add_deal_button = tk.Button(self.top, text="Add deal", command=self.create_deal)
 		self.button = tk.Button(self.top, text="OK", command=self.create_ddex)
+
+	def create_deal(self):
+		deal_window = DealWindow(self.top)
+		deal_window.wait_window()
+		deal = deal_window.create_deal()
+		self._release_builder.add_deal(deal)
 
 	def create_ddex(self):
 		self.__check_for_party(PartyType.MessageSender)
