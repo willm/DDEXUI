2c426cf923797e6f38c5dd25c4f877099691ae8f
Will Munn <willmunn@hotmail.com>
2013-12-30 13:28:23 +0000

    started implementing adding image resources

--- ddex/tests/test_resource_manager.py
+++ ddex/tests/test_resource_manager.py
@@ -7,7 +7,7 @@ from DDEXUI.file_parser import FileParser
 from DDEXUI.ddex.resource import SoundRecording
 from DDEXUI.resource_manager import ResourceManager
 
-class ResourceManagerTests(unittest.TestCase):
+class ResourceManagerSoundRecordingTests(unittest.TestCase):
 	def setUp(self):
 		self.upc = "49024343245"
 		self.isrc = "FR343245"
@@ -22,7 +22,7 @@ class ResourceManagerTests(unittest.TestCase):
 
 		self.subject = ResourceManager(FileParser(), self.batch_id, self.upc, self.root_folder)
 
-		self.resource = self.subject.add_resource(file_path, self.isrc, self.title)
+		self.resource = self.subject.add_sound_recording(file_path, self.isrc, self.title)
 
 	def test_should_copy_file_to_product_resources_folder(self):
 		expected_path = path.join(self.root_folder, self.batch_id, self.upc, 'resources', self.isrc+'.mp3')
@@ -39,3 +39,38 @@ class ResourceManagerTests(unittest.TestCase):
 
 	def test_should_create_resource_with_file(self):
 		self.assertEqual(self.resource.file_metadata.md5, self.expected.file_metadata.md5)
+
+
+class ResourceManagerImageTests(unittest.TestCase):
+	def setUp(self):
+		self.upc = "49024343245"
+		self.isrc = "FR343245"
+		rmtree(self.upc, ignore_errors=True)
+		self.root_folder = gettempdir()
+		self.batch_id = str(uuid.uuid4())
+		self.title = "the title"
+		file_path = path.join('ddex','tests','resources','test.jpg')
+
+		self.expected = SoundRecording('',self.isrc, self.title, FileParser().parse(file_path), '')
+
+		self.subject = ResourceManager(FileParser(), self.batch_id, self.upc, self.root_folder)
+
+		self.resource = self.subject.add_image(file_path)
+
+	def test_should_copy_file_to_product_resources_folder(self):
+		expected_path = path.join(self.root_folder, self.batch_id, self.upc, 'resources', self.upc+'.jpg')
+		self.assertTrue(path.isfile(expected_path))
+
+"""
+	def test_should_create_resource_with_isrc(self):
+		self.assertEqual(self.resource.isrc, self.expected.isrc)
+
+	def test_should_create_resource_with_title(self):
+		self.assertEqual(self.resource.title, self.expected.title)
+
+	def test_should_create_resource_with_title(self):
+		self.assertEqual(self.resource.title, self.expected.title)
+
+	def test_should_create_resource_with_file(self):
+		self.assertEqual(self.resource.file_metadata.md5, self.expected.file_metadata.md5)
+"""
