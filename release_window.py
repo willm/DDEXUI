a4a64b67133bf576281b97936d4b7b4981705686
Will Munn <willmunn@hotmail.com>
2014-01-12 23:58:55 +0000

    moved release window ui logic to product service

--- release_window.py
+++ release_window.py
@@ -82,32 +82,16 @@ class ProductReleaseWindow(ReleaseWindow):
 				.artist(self.value_of("Artist"))
 				.label(self.value_of("Label"))
 				.parental_warning(self.value_of("Explicit")))
-		if(self.image_path != None):
-			image = self._resource_manager.add_image(upc, self.image_path, "A0")
-			self._release_builder.add_resource(image.resource_reference())
-			self.ddex_builder.add_resource(image)
 
 	def create_ddex(self):
 		upc = self.value_of("UPC")
 		self._populate_product_release(upc)
-		self.ddex_builder.update(self.is_update_check_box.value())
-		count = 1
-		self.resource_count = 1
-		for track in self.track_builder_file_paths:
-			self._add_audio_resources(upc, track.paths, track.builder)
-			self.ddex_builder.add_release(track.builder.reference("R" + str(count)).build())
-			count += 1
-		self.ddex_builder.add_release(self._release_builder.build())
-		return self.ddex_builder
-	
-	def _add_audio_resources(self, upc, file_paths, builder):
-		for path in file_paths:
-			resource = self._resource_manager.add_sound_recording(upc, path, builder.get_isrc(), builder.get_title(), "A"+str(self.resource_count))
-			self.resource_count += 1
-			builder.add_resource(resource.resource_reference())
-			self._release_builder.add_resource(resource.resource_reference())
-			self.ddex_builder.add_resource(resource)
-			
+		product_service = (ProductService(self._release_builder, 
+			upc, self.image_path, 
+			self.track_builder_file_paths, 
+			self.is_update_check_box.value(), 
+			self._resource_manager))
+		return product_service.create_ddex()
 
 	@showerrorbox
 	def create_track(self):
@@ -172,3 +156,39 @@ class TrackBuilderFilePath:
 	def __init__(self, paths, builder):
 		self.paths = paths
 		self.builder = builder
+
+class ProductService:
+	def __init__(self, product_release_builder, upc, coverart_path, track_builder_file_paths, is_update, resource_manager):
+		self._product_release_builder = product_release_builder
+		self.upc = upc
+		self.coverart_path = coverart_path
+		self.track_builder_file_paths = track_builder_file_paths
+		self.is_update = is_update	
+		self._resource_manager = resource_manager
+		self.ddex_builder = DDEXBuilder()
+		self.ddex_builder.update(self.is_update)
+
+	def _add_image(self, upc):
+		if(self.coverart_path != None):
+			image = self._resource_manager.add_image(upc, self.coverart_path, "A0")
+			self._product_release_builder.add_resource(image.resource_reference())
+			self.ddex_builder.add_resource(image)
+
+	def _add_audio_resources(self, upc, file_paths, builder):
+		for path in file_paths:
+			resource = self._resource_manager.add_sound_recording(upc, path, builder.get_isrc(), builder.get_title(), "A"+str(self.resource_count))
+			self.resource_count += 1
+			builder.add_resource(resource.resource_reference())
+			self._product_release_builder.add_resource(resource.resource_reference())
+			self.ddex_builder.add_resource(resource)
+
+	def create_ddex(self):
+		self._add_image(self.upc)
+		count = 1
+		self.resource_count = 1
+		for track in self.track_builder_file_paths:
+			self._add_audio_resources(self.upc, track.paths, track.builder)
+			self.ddex_builder.add_release(track.builder.reference("R" + str(count)).build())
+			count += 1
+		self.ddex_builder.add_release(self._product_release_builder.build())
+		return self.ddex_builder
